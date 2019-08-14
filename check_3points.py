f8c3bb7755595a56f234bcc91ba32f7f2e237053
venkatvenkat-svg <53077792+venkatvenkat-svg@users.noreply.github.com>
2019-08-14 22:08:17 +0530

    Update check_3points.py

--- check_3points.py
+++ check_3points.py
@@ -1,7 +1,10 @@
 x1,y1=map(int,input().split())
 x2,y2=map(int,input().split())
 x3,y3=map(int,input().split())
-if(x1==x2 or x1==x3 or x2==x3 or y1==y2 or y1==y3 or y2==y3 ):
+m1=(y2-y1)//(x2-x1)
+m2=(y3-y2)//(x3-x2)
+m3=(y3-y1)//(x3-x1)
+if(m1==m2==m3):
     print("yes")
 else:
     print("no")
